080d76dc3c2d843e1643534572a65705513bc3e7
Jon Maestas <jonathan.maestas@outlook.com>
2022-04-06 17:06:23 +0000

    feat: configure gitpod and vite reload

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,21 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    vite: {
+      server: {
+        hmr: {
+          clientPort: process.env.HMR_HOST ? 443 : 3000,
+          host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+        }
+      }
+    }
+  }
 };
 
 export default config;
